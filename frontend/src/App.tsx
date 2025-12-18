6cf2712e77c3931444316905170fb70a4d81cdf1
SpencerRWood <Spencer.R.Wood@gmail.com>
2025-12-18 16:10:55 -0500

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/layouts/app-shell";
+import { LandingPage } from "@/pages/landing";
+
+export default function App() {
+  return (
+    <AppShell>
+      <LandingPage />
+    </AppShell>
+  );
+}
